785bdafcc8bd368213950285a40ac3e7a36b3aa3
giomelchors <giomelchors@gmail.com>
2023-06-08 11:45:11 -0500

    fisrt UI

--- proyecto_screenplay/src/main/java/screenplay/ui/sahetestsite/BooksInStockPage.java
+++ proyecto_screenplay/src/main/java/screenplay/ui/sahetestsite/BooksInStockPage.java
@@ -9,5 +9,5 @@ public class BooksInStockPage extends PageObject {
     public static final Target LB_CANTIDA_RUBY = Target.the("Campo cantidad  de libros Ruby for Rails").located(By.xpath("//*[@id=\"listing\"]/tbody/tr[3]/td[4]/input"));
     public static final Target LB_CANTIDA_PYTHON = Target.the("Campo cantidad  de libros Python Cookbook").located(By.xpath("//*[@id=\"listing\"]/tbody/tr[4]/td[4]/input"));
     public static final Target BTN_AGREGAR = Target.the("Boton agregar libros al carrito").located(By.xpath("//*[@id=\"available\"]/input[1]"));
-   
+
 }
--- proyecto_screenplay/src/main/java/screenplay/ui/sahetestsite/MyCardPage.java
+++ proyecto_screenplay/src/main/java/screenplay/ui/sahetestsite/MyCardPage.java
@@ -5,7 +5,6 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class MyCardPage extends PageObject {
-    public static final Target QUANTITY = Target.the("Libros en el carrito").locatedBy("//tr['{0}']/td[2]");
-    public static final Target TOTAL_COST = Target.the("Valor total por libros").locatedBy("//*[@id=\'added\']/tbody/tr['{0}']/td[4]");
+    public static final Target TOTAL_COST = Target.the("Valor total por libros").locatedBy("//*[@id='added']/tbody/tr['{0}']/td[4]");
     public static final Target GRAN_TOTAL = Target.the("Valor total de los libros").located(By.id("total"));
 }
